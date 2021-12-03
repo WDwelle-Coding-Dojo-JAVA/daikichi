6e1b42cfede5770d3bed8940b7d78bb421450818
WDwelle <willdwelle@frontier.com>
2021-12-03 12:21:19 -0800

    lottol and travel added

--- src/main/java/com/wdwelle/daikichi/HomeDaikichi.java
+++ src/main/java/com/wdwelle/daikichi/HomeDaikichi.java
@@ -1,5 +1,7 @@
 package com.wdwelle.daikichi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,18 @@ public class HomeDaikichi {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow you will gain new oppurtunities!";
+	}
+	@GetMapping("/travel/{local}")
+	public String travel(@PathVariable("local") String local) {
+		return "congratulations you will soon travel to " + local;
+	}
+	@GetMapping("/lotto/{num}")
+	public String lotto(@PathVariable("num") int num) {
+		if(num%2==0) {
+			return "You will take a grand journey in the near future, but be weary of tempting ofers";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends";
+		}
 	}
 }
